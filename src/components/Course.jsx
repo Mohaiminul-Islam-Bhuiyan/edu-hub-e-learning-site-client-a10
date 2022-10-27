703e491c42f6cc2b2002081741f17c58cd0e5d25
Mohaiminul-Islam-Bhuiyan <rafe.aust@gmail.com>
2022-10-27 10:51:42 +0600

    course details added

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import CourseDetails from './CourseDetails';
 
 const Course = ({ course }) => {
     const { id, fee, picture, name, instructor } = course
@@ -13,12 +12,12 @@ const Course = ({ course }) => {
             <div>
             <div className="card w-full bg-base-100 shadow-xl">
                 <figure className="px-2 pt-2">
-                    <img src={picture} alt="Courses" className="rounded-xl" />
+                    <img src={picture} alt="Courses" className="rounded-xl h-60" />
                 </figure>
                 <div className="card-body items-center text-center">
                     <h2 className="card-title">{name}</h2>
                     <p>{instructor}</p>
-                    <p>${fee}</p>
+                    <p>{fee}</p>
                     <div className="card-actions">
                         <button className="btn btn-primary"><Link to={`/courses/${id}`} >See Details </Link></button>
                     </div>
--- src/components/CourseDetails.jsx
+++ src/components/CourseDetails.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import LeftSideNavbar from './LeftSideNavbar';
 
 const CourseDetails = () => {
+    const details = useLoaderData()
+    const { id, fee, picture, name, instructor, about } = details
     return (
-        <div>
-            <h2>this is course details</h2>
+        <div className='flex m-5'>
+            <div className='grow-0 mr-3'>
+                <LeftSideNavbar></LeftSideNavbar>
+            </div>
+            <div className='grow'>
+                <div className='grid grid-cols-2'>
+                    <div>
+                        <img src={picture} alt="" />
+                        <h2>{name}</h2>
+                        <p>{instructor}</p>
+                        <p><small>{fee}</small></p>
+                    </div>
+                    <div className='my-auto mx-5'>
+                        <h3>{about}</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
