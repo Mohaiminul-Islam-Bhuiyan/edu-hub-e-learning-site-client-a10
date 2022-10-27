995cabfa496c7064f4dc0ceb87f5c9bf1926dd70
Mohaiminul-Islam-Bhuiyan <rafe.aust@gmail.com>
2022-10-27 16:55:15 +0600

    print page added

--- src/components/CourseDetails.jsx
+++ src/components/CourseDetails.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { useRef } from 'react';
 import { FaArrowRight } from 'react-icons/fa';
 import { Link, useLoaderData } from 'react-router-dom';
+import ReactToPrint from 'react-to-print';
 import LeftSideNavbar from './LeftSideNavbar';
 
 const CourseDetails = () => {
+    const componentRef = useRef()
     const details = useLoaderData()
-    const {id, fee, picture, name, instructor, about, duration } = details
+    const { id, fee, picture, name, instructor, about, duration } = details
     return (
+        <>
         <div className='flex m-5'>
             <div className='grow-0 mr-3'>
                 <LeftSideNavbar></LeftSideNavbar>
             </div>
-            <div className='grow'>
+            <div ref={componentRef} className='grow'>
                 <div className='grid grid-cols-2'>
                     <div>
                         <img src={picture} alt="" />
@@ -22,11 +26,23 @@ const CourseDetails = () => {
                     </div>
                     <div className='my-auto mx-5'>
                         <h3>{about}</h3>
-                        <button className="btn btn-wide btn-primary mt-5"><Link to={`/courses/${id}/checkout`}>Checkout</Link><FaArrowRight></FaArrowRight></button>
+                        
                     </div>
                 </div>
             </div>
         </div>
+        <div>
+       <div>
+       <ReactToPrint
+            trigger={() => <button className="btn btn-wide btn-primary mt-5">Print this page</button>}
+            content={() => componentRef.current}
+        />
+       </div>
+        <>
+        <button className="btn btn-wide btn-primary mt-5 mb-5"><Link to={`/courses/${id}/checkout`}>Checkout</Link><FaArrowRight></FaArrowRight></button>
+        </>
+    </div>
+    </>
     );
 };
 
--- src/components/Login/Register.jsx
+++ src/components/Login/Register.jsx
@@ -1,48 +1,54 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
     return (
         <div>
             <div className='ml-40 mr-40 mt-20 md:mx-96'>
-            <div className="">
-                <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                            Username
-                        </label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Please enter your full name" />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                            Email
-                        </label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Please enter your email" />
-                    </div>
-                    <div className="mb-6">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                            Password
-                        </label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"  id="password" type="password" placeholder="******************" />
-                        <p className="text-red-500 text-xs italic">Please choose a password carefully.</p>
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                            Confirm Password
-                        </label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="password" placeholder="******************" />
-                    </div>
+                <div className="">
+                    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                                Username
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Please enter your full name" />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                                Photo URL
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Please enter your photo URL here" />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                                Email
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Please enter your email" />
+                        </div>
+                        <div className="mb-6">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                                Password
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+                            <p className="text-red-500 text-xs italic">Please choose a password carefully.</p>
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                                Confirm Password
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="password" placeholder="******************" />
+                        </div>
 
-                    <button className="mb-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                        <button className="mb-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                             Register
                         </button>
-                        <p className='text-sm'>Already have an account? Please <Link to= '/login' className='font-bold  text-blue-500 hover:text-blue-800'>Login</Link></p>
-                </form>
-                <p className="text-center text-gray-500 text-xs">
-                    &copy;2022 EduHub. All rights reserved.
-                </p>
+                        <p className='text-sm'>Already have an account? Please <Link to='/login' className='font-bold  text-blue-500 hover:text-blue-800'>Login</Link></p>
+                    </form>
+                    <p className="text-center text-gray-500 text-xs">
+                        &copy;2022 EduHub. All rights reserved.
+                    </p>
+                </div>
             </div>
-        </div>
         </div>
     );
 };
